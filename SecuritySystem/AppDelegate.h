ba991e54595714ec98a46bc16f65e187eedd7eba
王琨 <wangkun@kkingbox.local>
2017-12-14 09:40:06 +0800

    Initial Commit

---
+++ SecuritySystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecuritySystem
+//
+//  Created by 王琨 on 2017/12/14.
+//  Copyright © 2017年 wangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecuritySystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecuritySystem
+//
+//  Created by 王琨 on 2017/12/14.
+//  Copyright © 2017年 wangkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
